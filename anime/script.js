dfc2135b78d2a2e0b340306f626b73cc9777e442
老猫 <141402281+oldcatmiku@users.noreply.github.com>
2024-08-15 13:03:43 +0800

    大修改

--- anime/script.js
+++ anime/script.js
@@ -1,102 +1,147 @@
 var animeList= [
 {
-    "name":"囮物语 <a href='https://www.acfun.cn/bangumi/aa6003043_36188_1738667'> 第2地址 </a>",
-	"img":"https://cdn.yinghuazy.xyz/upload/vod/20201029-3/6f0c0360bd0ba209cb259ea0808db907.jpg", 
-    "url":"http://www.wedm3.com/video/3903.html",
+    "name": "玉子市场 ",
+    "img": "https://i0.hdslb.com/bfs/bangumi/67da3dae76e526a925b78b1d8abe21c870333491.jpg@330w_442h.webp",
+    "url": "https://www.bilibili.com/bangumi/media/md116772",
+    "introduce": "座落某个小镇的兔子商店街上，有一间日式饼店，住着一位十分喜欢饼类小吃的高中一年级女生——玉子。除了偶尔在店铺协助父亲，以及钻研新口味的饼类小吃外，她亦有跟学校的朋友参与羽毛球部活动。而饼店对面是同行的竞争对手，两边的父亲经常都因生意问题而喧哗。不过对方儿子——饼藏却自小跟玉子建立青梅竹马的关系，近日更不断隐约地表达心思，只是玉子没有特别注意，更常常被旁观的朋友拿出来扰攘一番。总而言之，玉子就是在商店街众人的护荫下，如此热闹地渡过每一天，生活可谓过得相当快乐。",
+    "catalog": ""
+},
+{
+    "name": "白圣女与黑牧师 ",
+    "img": "https://image.baidu.com/search/down?url=https://lz.sinaimg.cn/large/006yt1Omgy1h6huxwpc97j30ku0ti77a.jpg",
+    "url": "https://www.wedm3.com/video/7614.html",
+    "introduce": "为了照顾一个人什么也不做、无所事事的圣女·塞西莉娅，性格一板一眼的牧师·劳伦斯不得不时刻忙碌着…… 完全相反的两个人彼此尊敬着对方，之间偶尔也会产生不错的气氛……？投稿在pixiv上的，我行我素圣女×费力劳心牧师的同居纯爱喜剧！",
+    "catalog": "<dd style=\"display:block;\"><a class=\"on\" href=\"/play/7614-1-1.html\" title=\"第一线路:第01集\"><p>第01集</p></a><a href=\"/play/7614-1-2.html\" title=\"第一线路:第02集\"><p>第02集</p></a><a href=\"/play/7614-1-3.html\" title=\"第一线路:第03集\"><p>第03集</p></a><a href=\"/play/7614-1-4.html\" title=\"第一线路:第04集\"><p>第04集</p></a><a href=\"/play/7614-1-5.html\" title=\"第一线路:第05集\"><p>第05集</p></a><a href=\"/play/7614-1-6.html\" title=\"第一线路:第06集\"><p>第06集</p></a><a href=\"/play/7614-1-7.html\" title=\"第一线路:第07集\"><p>第07集</p></a><a href=\"/play/7614-1-8.html\" title=\"第一线路:第08集\"><p>第08集</p></a><a href=\"/play/7614-1-9.html\" title=\"第一线路:第09集\"><p>第09集</p></a><a href=\"/play/7614-1-10.html\" title=\"第一线路:第10集\"><p>第10集</p></a><a href=\"/play/7614-1-11.html\" title=\"第一线路:第11集\"><p>第11集</p></a><a href=\"/play/7614-1-12.html\" title=\"第一线路:第12集\"><p>第12集</p></a></dd>"
+},
+{
+    "name": "在魔王城说晚安",
+    "img": "https://www.dmttang.com/upload/vod/20220322-2/ba8eefe4b6a913d85f347c017fc71714.jpg",
+    "url": "https://www.dmttang.com/voddetail/717.html",
+    "introduce": "\n这是一个人类与魔族共存的世界，但是世界的支配权还是掌握在人类的受众。一天，魔王抓走并囚禁了人类公主栖夜莉丝（水濑祈 配音），并且向人类发出了勒索信，要求人类用世界的支配权来换回公主的自由。为了拯救公主，勇者拂晓（下野纮 配音）踏上了充满了危机的旅途。然而，被囚禁的日子实在是太过于无聊了，古灵精怪又闲不住的栖夜莉丝决定在魔王城里找点乐子。无论是恶魔教会的法师雷欧纳多（石川界人 配音）还是世代侍奉魔王的忠犬哈士奇（小林亲弘 配音），甚至连威风凛凛的魔王（松冈祯丞 配音）本人，都被公主的恶作剧给耍得团团转。",
 },
 {
     "name":"Mygo!!!!!",
 	"img":"https://lain.bgm.tv/pic/cover/l/e7/a7/428735_1v11n.jpg", 
     "url":"https://www.bilibili.com/video/BV1qJ4m1M7jp",
+    "introduce":"《BanG Dream! It's MyGO!!!!!》是《BanG Dream!》的新作动画作品“你能，和我组一辈子乐队吗？”高一的春末。羽丘女子学园里的学生基本上都组好了乐队，推迟入学的爱音也在为了能尽快融入班级而急忙寻找乐队成员。在这时候，知道「羽丘的小怪人」灯还没组乐队之后，爱音不由自主地向她搭话……遍体鳞伤且蓬头垢面的，我们的“音乐（呐喊）”。不畏迷茫，纵使迷茫亦要前行。",
+    "catalog":`<dd style="display:block;"><a class="on" href="/play/8322-1-1.html" title="第一线路:第01集"><p>第01集</p></a><a href="/play/8322-1-2.html" title="第一线路:第02集"><p>第02集</p></a><a href="/play/8322-1-3.html" title="第一线路:第03集"><p>第03集</p></a><a href="/play/8322-1-4.html" title="第一线路:第04集"><p>第04集</p></a><a href="/play/8322-1-5.html" title="第一线路:第05集"><p>第05集</p></a><a href="/play/8322-1-6.html" title="第一线路:第06集"><p>第06集</p></a><a href="/play/8322-1-7.html" title="第一线路:第07集"><p>第07集</p></a><a href="/play/8322-1-8.html" title="第一线路:第08集"><p>第08集</p></a><a href="/play/8322-1-9.html" title="第一线路:第09集"><p>第09集</p></a><a href="/play/8322-1-10.html" title="第一线路:第10集"><p>第10集</p></a><a href="/play/8322-1-11.html" title="第一线路:第11集"><p>第11集</p></a><a href="/play/8322-1-12.html" title="第一线路:第12集"><p>第12集</p></a><a href="/play/8322-1-13.html" title="第一线路:第13集"><p>第13集</p></a></dd>`,
 },
 {
     "name":"哭泣少女乐队",
 	"img":"anime/posters/03.jpg", 
     "url":"https://www.wedm3.com/video/13080.html",
+    "introduce":"“愤怒、喜悦、悲伤全部倾注！”主人公在高中2年级从学校退学，决定只身前往东京读大学。被同伴背叛而不知如何是好的少女。被父母抛弃，独自在大城市一个人打工的少女。虽然这个世界总是背叛我们，什么都不如意。即便如此，我们依然有着热爱的事物。因为我们相信自己的一席之地就在世界的某个角落。所以，歌唱吧。",
+    "catalog":`<dd style="display:block;"><a href="/play/13080-1-1.html" title="第一线路:第01集"><p>第01集</p></a><a class="on" href="/play/13080-1-2.html" title="第一线路:第02集"><p>第02集</p></a><a href="/play/13080-1-3.html" title="第一线路:第03集"><p>第03集</p></a><a href="/play/13080-1-4.html" title="第一线路:第04集"><p>第04集</p></a><a href="/play/13080-1-5.html" title="第一线路:第05集"><p>第05集</p></a><a href="/play/13080-1-6.html" title="第一线路:第06集"><p>第06集</p></a><a href="/play/13080-1-7.html" title="第一线路:第07集"><p>第07集</p></a><a href="/play/13080-1-8.html" title="第一线路:第08集"><p>第08集</p></a><a href="/play/13080-1-9.html" title="第一线路:第09集"><p>第09集</p></a><a href="/play/13080-1-10.html" title="第一线路:第10集"><p>第10集</p></a><a href="/play/13080-1-11.html" title="第一线路:第11集"><p>第11集</p></a><a href="/play/13080-1-12.html" title="第一线路:第12集"><p>第12集</p></a><a href="/play/13080-1-13.html" title="第一线路:第13集"><p>第13集</p></a></dd>`
 },
 {
 	"name":"莉兹与青鸟",
 	"img":"anime/posters/01.jpg", 
     "url":"https://pan.baidu.com/s/1JkIZCspH0Ytv0Hn_YqoDKA?pwd=jwek",
-},
-{
-	"name":"孤独摇滚！",
-	"img":"https://image.baidu.com/search/down?url=https://lz.sinaimg.cn/large/006yt1Omgy1h6ns614olkj30no0xcn5c.jpg", 
-    "url":"https://www.wedm3.com/video/1694.html",
+    "introduce":"《莉兹与青鸟》是京都动画以《吹响！上低音号》为原作制作的动画电影。作品于2018年4月21日在日本上映。北宇治高中管乐团负责双簧管的铠冢霙与负责长笛的伞木希美，高中三年级是两人最后的大赛，被选为自由曲的《莉兹与青鸟》有着双簧管与长笛的共演独奏，霙对能与希美一同度日感到幸福，同时也害怕这样的日子将会迎来结束。本来应该是“好友”的两人，双簧管与长笛的独奏却无法顺利契合，两人也感觉的彼此之间有些距离。",
 },
 {
 	"name":"轻音少女第二季",
-	"img":"https://img2.imgtp.com/2024/03/31/i0NirBdz.png", 
+	"img":"https://cdn.yinghuazy.xyz/upload/vod/20201029-3/9e57bde0c3f1ae7dcdbe77c9c65f6af0.jpg", 
     "url":"https://www.wedm3.com/video/778.html",
+    "introduce":"故事发生在樱丘高中，围绕着由五个成员组成的轻音部展开。这支名为“放学后茶会”的乐队中，担任吉他和主唱的是天然呆少女平沢唯（丰崎爱生 配音），担任贝斯手的是稳重但容易害羞的秋山澪（日笠阳子 配音），担任鼓手的部长，是非常有活力的田井中律（佐藤聪美 配音），温柔热情的键盘手是琴吹紬（寿美菜子 配音），吉他手是学妹中野梓（竹达彩奈 配音）。新学年开始，除了小梓，大家都升上三年级，还被分到了同一个班级。然而要是再不招揽新成员入部，来年四名成员毕业离校，轻音部就要面临废部的命运了。新学期樱高的各大社团都开始紧锣密鼓地招新，轻音部也使尽了各种办法，她们在新生欢迎会上的演出，能否吸引新人加入呢？K-ON!的青春故事在第二季中继续进行！"
 },
 {
 	"name":"凉宫春日的忧郁 2009",
-	"img":"https://img2.imgtp.com/2024/03/31/nIaIbkre.png", 
+	"img":"https://cdn.yinghuazy.xyz/upload/vod/20201029-4/40258a70b41a79e6af063f04d467dfdc.jpg", 
     "url":"https://www.wedm3.com/video/4535.html",
+    "introduce":"2007年7月7日，“SOS团官方网站”施行“七夕”活动，《朝日新闻》早报上出现“凉宫春日第2期决定制作！”全页广告。2007年12月18日，“SOS团官方网站”施行“消失”活动，宣布“第2期中止。将以此为基础决定制作全新动画”。但是在2008年8月26日，《少年ACE》公布的却是新动画《小凉宫春日的忧郁》&《喵☆小鹤屋学姐》动画版制作决定。在2009年2月6日发售的《Newtype》3月号上，正式宣布了TV动画“凉宫春日第2期”的制作决定。 《凉宫春日的忧郁（2009年版）》于2009年4月2日开始“从头”播放，由十四集新作和2006年版的十四集交叉播出。其中的新作《漫无止境的八月》更是忠实原著情节，以每集更换导演、重新作画及录音的独特表现重演了八次，成功诠释依春日之愿不断重复的暑假。"
 },
 {
-	"name":"紫罗兰永恒花园",
-	"img":"https://cdn.yinghuazy.xyz/upload/vod/20210108-1/23bc648cb1408fcd0fa159d677e9c34e.jpg", 
-    "url":"https://www.wedm3.com/video/139220.html",
+    "name": "紫罗兰永恒花园",
+    "img": "https://cdn.yinghuazy.xyz/upload/vod/20210108-1/23bc648cb1408fcd0fa159d677e9c34e.jpg",
+    "url": "https://www.wedm3.com/video/3922.html",
+    "introduce": "某个大陆的、某个时代。 大陆南北分割的战争结束了，世界走向了和平。 在战争中作为军人的薇尔莉特•伊芙加登，怀抱着对她来说无比重要之人留下的“话语”，离开军队来到了大港口城市。 踊跃的人群在排列着煤气灯的街道马路上来来往往地穿梭着。薇欧瑞特在街道上找到了“代写书信”的工作。那是根据委托人的想法来组织出相应语言的工作。 她直面委托人、触碰着他们内心深处的坦率感情。与此同时，薇欧瑞特在记录书信时，渐渐明白那“话语”的含义。",
+    "catalog": "<dd style=\"display:block;\"><a class=\"on\" href=\"/play/3922-1-1.html\" title=\"第一线路:第01集\"><p>第01集</p></a><a href=\"/play/3922-1-2.html\" title=\"第一线路:第02集\"><p>第02集</p></a><a href=\"/play/3922-1-3.html\" title=\"第一线路:第03集\"><p>第03集</p></a><a href=\"/play/3922-1-4.html\" title=\"第一线路:第04集\"><p>第04集</p></a><a href=\"/play/3922-1-5.html\" title=\"第一线路:第05集\"><p>第05集</p></a><a href=\"/play/3922-1-6.html\" title=\"第一线路:第06集\"><p>第06集</p></a><a href=\"/play/3922-1-7.html\" title=\"第一线路:第07集\"><p>第07集</p></a><a href=\"/play/3922-1-8.html\" title=\"第一线路:第08集\"><p>第08集</p></a><a href=\"/play/3922-1-9.html\" title=\"第一线路:第09集\"><p>第09集</p></a><a href=\"/play/3922-1-10.html\" title=\"第一线路:第10集\"><p>第10集</p></a><a href=\"/play/3922-1-11.html\" title=\"第一线路:第11集\"><p>第11集</p></a><a href=\"/play/3922-1-12.html\" title=\"第一线路:第12集\"><p>第12集</p></a><a href=\"/play/3922-1-13.html\" title=\"第一线路:第13集\"><p>第13集</p></a></dd>"
 },
-{
-	"name":"伪物语",
-	"img":"https://img2.imgtp.com/2024/03/31/5Kxrru22.png", 
-    "url":"https://www.acfun.cn/bangumi/aa6004318",
-},
-{
-	"name":"猫物语（黑）",
-	"img":"https://cdn.yinghuazy.xyz/upload/vod/20201029-4/f8f1d16928cdce15c80b6f998019067a.jpg", 
-    "url":"https://www.acfun.cn/bangumi/aa6004319_36188_1754217",
-},
-{
-	"name":"猫物语（白）",
-	"img":"https://th.bing.com/th/id/R.ac4b457283566374e08ae3c9da91f52e?rik=aY0Ex43LgtP%2fug&riu=http%3a%2f%2fi1.hdslb.com%2fbfs%2farchive%2fabef8dbb4a6f58b869bb17d18c8c4ea6a86cf3a4.jpg&ehk=Mi6B9QMpWj1swZfPY7uCci6o65jFdZN4k1wkxQIYiqY%3d&risl=&pid=ImgRaw&r=0", 
-    "url":"https://www.acfun.cn/bangumi/aa6002986_36188_1737690",
-},
-{
-    "name":"倾物语 <a href='https://www.wedm3.com/video/131720.html'> 第2地址 </a>",
-	"img":"anime/posters/02.jpg", 
-    "url":"https://www.acfun.cn/bangumi/aa6002987",
-},
-{
-    "name":"囮物语 <a href='https://www.wedm3.com/video/131730.html'> 第2地址 </a>",
-	"img":"https://tse4-mm.cn.bing.net/th/id/OIP-C.7WWDzMekbbwLbDccEMhkFAAAAA?w=122&h=180&c=7&r=0&o=5&dpr=1.3&pid=1.7", 
-    "url":"https://www.acfun.cn/bangumi/aa6002988_36188_1737694",
-},
-{
-    "name":"憧憬成为魔法少女",
-	"img":"anime/posters/04.webp", 
-    "url":"https://www.wedm3.com/video/1110730.html",
-},
-{
-	"name":"吹响！上低音号",
-	"img":"https://cdn.yinghuazy.xyz/upload/vod/20201029-3/abffd7d0b23cd1264008ec3dbada0eb4.jpg", 
-    "url":"https://www.wedm3.com/video/147900.html",
-},
-{
-	"name":"打了300年的史莱姆 不知不觉就练到了满级",
-	"img":"https://cdn.yinghuazy.xyz/jpg/ly1h2yppazilfj30ew0l87ac.jpg", 
-    "url":"https://www.wedm3.com/video/19720.html",
-},
-{
-	"name":"情色漫画老师",
-	"img":"https://cdn.yinghuazy.xyz/upload/vod/20201029-3/ab0f6dc975b43006464d206ac385c0ca.jpg", 
-    "url":"https://www.wedm3.com/video/142990.html",
-},
+// {
+//     "name":"囮物语 <a href='https://www.acfun.cn/bangumi/aa6003043_36188_1738667'> 第2地址 </a>",
+// 	"img":"https://cdn.yinghuazy.xyz/upload/vod/20201029-3/6f0c0360bd0ba209cb259ea0808db907.jpg", 
+//     "url":"http://www.wedm3.com/video/3903.html",
+// },
+// {
+// 	"name":"孤独摇滚！",
+// 	"img":"https://image.baidu.com/search/down?url=https://lz.sinaimg.cn/large/006yt1Omgy1h6ns614olkj30no0xcn5c.jpg", 
+//     "url":"https://www.wedm3.com/video/1694.html",
+// },
+// {
+// 	"name":"伪物语",
+// 	"img":"https://img2.imgtp.com/2024/03/31/5Kxrru22.png", 
+//     "url":"https://www.acfun.cn/bangumi/aa6004318",
+// },
+// {
+// 	"name":"猫物语（黑）",
+// 	"img":"https://cdn.yinghuazy.xyz/upload/vod/20201029-4/f8f1d16928cdce15c80b6f998019067a.jpg", 
+//     "url":"https://www.acfun.cn/bangumi/aa6004319_36188_1754217",
+// },
+// {
+// 	"name":"猫物语（白）",
+// 	"img":"https://th.bing.com/th/id/R.ac4b457283566374e08ae3c9da91f52e?rik=aY0Ex43LgtP%2fug&riu=http%3a%2f%2fi1.hdslb.com%2fbfs%2farchive%2fabef8dbb4a6f58b869bb17d18c8c4ea6a86cf3a4.jpg&ehk=Mi6B9QMpWj1swZfPY7uCci6o65jFdZN4k1wkxQIYiqY%3d&risl=&pid=ImgRaw&r=0", 
+//     "url":"https://www.acfun.cn/bangumi/aa6002986_36188_1737690",
+// },
+// {
+//     "name":"倾物语 <a href='https://www.wedm3.com/video/131720.html'> 第2地址 </a>",
+// 	"img":"anime/posters/02.jpg", 
+//     "url":"https://www.acfun.cn/bangumi/aa6002987",
+// },
+// {
+//     "name":"囮物语 <a href='https://www.wedm3.com/video/131730.html'> 第2地址 </a>",
+// 	"img":"https://tse4-mm.cn.bing.net/th/id/OIP-C.7WWDzMekbbwLbDccEMhkFAAAAA?w=122&h=180&c=7&r=0&o=5&dpr=1.3&pid=1.7", 
+//     "url":"https://www.acfun.cn/bangumi/aa6002988_36188_1737694",
+// },
+// {
+//     "name":"憧憬成为魔法少女",
+// 	"img":"anime/posters/04.webp", 
+//     "url":"https://www.wedm3.com/video/1110730.html",
+// },
+// {
+// 	"name":"吹响！上低音号",
+// 	"img":"https://cdn.yinghuazy.xyz/upload/vod/20201029-3/abffd7d0b23cd1264008ec3dbada0eb4.jpg", 
+//     "url":"https://www.wedm3.com/video/147900.html",
+// },
+// {
+// 	"name":"打了300年的史莱姆 不知不觉就练到了满级",
+// 	"img":"https://cdn.yinghuazy.xyz/jpg/ly1h2yppazilfj30ew0l87ac.jpg", 
+//     "url":"https://www.wedm3.com/video/19720.html",
+// },
+// {
+// 	"name":"情色漫画老师",
+// 	"img":"https://cdn.yinghuazy.xyz/upload/vod/20201029-3/ab0f6dc975b43006464d206ac385c0ca.jpg", 
+//     "url":"https://www.wedm3.com/video/142990.html",
+// },
 ];
 
+// 处理 animeList 数组中的每个元素
+const element = document.getElementById("output");
+
 for (let i = 0; i < animeList.length; i++) {
-    // 在此处处理每个元素
-    const element = document.getElementById("output");
+    // 检查是否存在 catalog 属性
+    let catalogContent;
+    
+    if (animeList[i].catalog) {
+        // 如果 catalog 存在，使用正则表达式将每个 "/play/" 前面加上域名
+        catalogContent = animeList[i].catalog.replace(/href="\/play\//g, 'href="https://www.wedm3.com/play/');
+    } else {
+        // 如果 catalog 不存在，显示“直接去视频网站观看”
+        catalogContent = '<br><center>👉 直接去视频网站观看❤️</center>';
+    }
+
+    // 将修改后的内容插入到页面中
     element.innerHTML += `
     <div class="bord">
-    <div class="imgs">
-        <a href="` + animeList[i].url + `">
-            <img src="` + animeList[i].img + `">
-        </a>
-    </div>
-    <span class="title">` + animeList[i].name + `</span>
-</div>
-    `;
+        <div class="imgs">
+            <a href="` + animeList[i].url + `">
+                <img src="` + animeList[i].img + `">
+            </a>
+        </div>
+        <div class="text">
+            <span class="title">` + animeList[i].name + `</span>
+            <span class="introduce">` + animeList[i].introduce + `</span>
+            <span>` + catalogContent + `</span>
+        </div>
+    </div>`;
 }
